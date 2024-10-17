031d412c5235a8c6345aa365b595f731ce14b203
Justin Jia <jiajia1015@gmail.com>
2024-10-17 11:22:09 +0800

    Update Weight_init_for_ReLu_networks.py

--- pages/Weight_init_for_ReLu_networks.py
+++ pages/Weight_init_for_ReLu_networks.py
@@ -29,7 +29,7 @@ css_str = """
 
 st.markdown(css_str, unsafe_allow_html=True)
 
-col1, col2 = st.columns([0.4, 0.6])
+col1, col2, _ = st.columns([0.3, 0.3, 0.4])
 
 no_of_nodes = col1.slider(r"Choose no. of nodes per layer", 50, 500, 100, step=50)
 sd = col1.slider(r"Choose standard deviation", 0.02, 0.2, 0.02, step=0.02)
@@ -51,7 +51,7 @@ if strategy == 'Naive initialization':
     st.markdown(r"Weights drawn randomly from $\mathcal{N} \sim" + f"(0, {sd})$")
 
 else:
-    st.markdown(r"Weights drawn randomly from $\mathcal{N} \sim (0,   \frac{1}{\sqrt{n^{[l]} /2}})$")
+    st.markdown(r"Weights drawn randomly from $\mathcal{N} \sim (0,   \frac{1}{\sqrt{n^{[l-1]} /2}})$")
 
 for i in range(len(hidden_layer_sizes)):
   X = D if i == 0 else Hs[i-1]
@@ -65,20 +65,27 @@ for i in range(len(hidden_layer_sizes)):
   H =  act[nonlinearities[i]](H)
   Hs[i] = H
 
-fig, axes = plt.subplots(2, 5, figsize=(10, 4))
+fig, axes = plt.subplots(2, 5, figsize=(7.5, 3))
 
 for ax, (i, H) in zip(axes.ravel(), Hs.items()):
     histogram = ax.hist(H.ravel(), 100, range=(-1, 1), color=(66/255, 133/255, 244/255, 1))
 
-    ax.set_title(f"Layer {i+1}", fontsize=8, pad=4)
-    ax.text(-0.98, histogram[0].max()*0.95, f"mean = {H.mean():.2f}", c='k', fontsize=8)
-    ax.text(-0.98, histogram[0].max()*0.85, f"std = {H.std():.2f}", c='k', fontsize=8)
+    ax.set_title(f"Layer {i+1}", fontsize=6, pad=4)
+    ax.text(-0.98, histogram[0].max()*0.95, f"mean = {H.mean():.2f}", c='k', fontsize=6)
+    ax.text(-0.98, histogram[0].max()*0.85, f"std = {H.std():.2f}", c='k', fontsize=6)
 
-    ax.tick_params(axis='both', which='major', labelsize=6, labelcolor="grey",
+    ax.tick_params(axis='both', which='major', labelsize=4, labelcolor="grey",
                   color="grey", length=2, width=0.5, pad=0.5)
     # ax.set_xlabel("Size", fontsize=14, labelpad=12)
     # ax.set_ylabel("No. of\n Bedrooms", fontsize=14, labelpad=12)
 
 
-plt.tight_layout()
-st.pyplot(fig, use_container_width=False)
+plt.subplots_adjust(left=0.05,
+                    bottom=0.05, 
+                    right=0.95, 
+                    top=0.95, 
+                    wspace=0.2, 
+                    hspace=0.2)
+
+
+st.pyplot(fig)
